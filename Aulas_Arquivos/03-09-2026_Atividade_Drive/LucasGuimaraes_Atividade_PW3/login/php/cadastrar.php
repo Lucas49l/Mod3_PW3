aa6cfd8c848ccd9c6a86a777f2c0e091820feb89
Lucas49l <lucasguimaraes1649@gmail.com>
2026-04-17 14:25:13 -0300

    Up pasta Aula 09/03/26 - Postar atividade no drive

---
+++ Aulas_Arquivos/03-09-2026_Atividade_Drive/LucasGuimaraes_Atividade_PW3/login/php/cadastrar.php
@@ -0,0 +1,29 @@
+<?php
+if( isset( $_POST['email'])){
+    $nome  = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    require 'Usuario.class.php';
+
+    $usuario = new Usuario();
+    $conn = $usuario->conectar();
+    if ($conn){
+
+        $userCheck = $usuario->checkUser($email);
+        if($userCheck){
+            header("Location:../index.html");
+            exit();
+        }
+
+        $user = $usuario->insertUser($nome, $email, $senha);
+        if( $user ){
+            echo "Usuario inserido com Sucesso!";
+        }else{
+            echo "Erro ao inserir o Usuario!!!";
+        }
+        
+    }else{
+        echo "Banco Indisponivel. Tente mais tarde!";
+    }
+}
